f25af2b965763a19124a1587e97acd65440dd1a9
Allen Xavier Arasan <xavierallem1999@gmail.com>
2020-09-19 16:47:38 +0200

    Update iftt.h

--- assignments/practice/problem1/iftt.h
+++ assignments/practice/problem1/iftt.h
@@ -1,32 +1,18 @@
-/** @file <file_name>.c
- *  @brief Give a description of the file 
- *  
- *  Give full description of the file 
- *  
- *  @author Full name of the author 
- *  @bug List Any bugs found in the file  
- */
-
-/*
- *#####################################################################
- *  Initialization block
- *  ---------------------
- *  This block contains initialization code for this particular file.
- *  It typically contains Includes, constants or global variables used
- *  throughout the file.
- *#####################################################################
- */
-
-/* --- Standard Includes --- */
-#include <stdio.h>
-
-/* --- Project Includes --- */
+#ifndef iftt.h
+#define iftt.h
 
 
-/*
- *#####################################################################
- *  Process block
- *  -------------
- *  Solve all your problems here 
- *#####################################################################
- */
+/*Linking*/
+extern void POST_IFTTT (const char *ptr,const char *ptr1)
+
+
+
+
+
+
+
+
+
+
+
+#endif
